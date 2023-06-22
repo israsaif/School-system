1734cc8d8e6f530e0bcaeeaa96096713c17c8812
israsaif <1616080@mazcol.edu.om>
2023-06-22 13:33:51 +0400

    Add api

--- src/main/java/com/School/Teacher/Controller/TeacherController.java
+++ src/main/java/com/School/Teacher/Controller/TeacherController.java
@@ -1,18 +1,56 @@
 package com.School.Teacher.Controller;
-
 import com.School.Teacher.Model.Teacher;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping(value = "api/t1/Teacher")
 public class TeacherController {
-    @PostMapping
-    Teacher createTeacher(@RequestBody Teacher createTeacher){
+
+
+
+CopyOnWriteArrayList<Teacher> listOfTeacher = new CopyOnWriteArrayList<>(); //
+
+
+
+    @PostMapping //create
+    Teacher createTeacher(@RequestBody Teacher addTeacher){
+        listOfTeacher.add(addTeacher);
         return new Teacher();
+    }
+
+    @GetMapping //GetAll
+    List<Teacher> getEveryTeacherInfo(){
+        return listOfTeacher;
+    }
+
+
+   @GetMapping(path = "{id}")
+   Teacher getTeacherInfo(@PathVariable(name="id") String incomingId){ //
+     Teacher currentTeacher = listOfTeacher.stream().filter((currTeacher)->{
+            return currTeacher.id.equals(incomingId);
+       }).findAny().get();
+        return currentTeacher;
+
+}
+
+
+    @PutMapping(path = "{id}")
+    Teacher updateTeacherInfo(@PathVariable(name="id") String incomingId, @RequestBody Teacher incomingupdateTeacher){
+       Teacher currentTeacher =getTeacherInfo(incomingId);
+        currentTeacher.name = incomingupdateTeacher.name;
+       return new Teacher();
+    }
+
+    @DeleteMapping(path = "{id}")
+    Teacher deleteTeacher(@PathVariable(name = "id")String id){
+        Teacher currentTeacher = getTeacherInfo(id);
+        listOfTeacher.remove(currentTeacher);
+        return currentTeacher;
+
     }
 
 }
